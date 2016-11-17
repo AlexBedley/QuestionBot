7c8b4bf8da551aca387f49b3cadb160a7438a5bb
Steffanie Freeman <Steffanie.Freeman@D2L.com>
2016-11-17 14:04:32 -0500

    Update unit test now that if statement for exit command has been removed from MessageEmitter

--- QuestionBot/QuestionBot.UnitTests/Model/MessageEmitterTests.cs
+++ QuestionBot/QuestionBot.UnitTests/Model/MessageEmitterTests.cs
@@ -21,19 +21,22 @@ namespace QuestionBot.UnitTests.Model {
         public void Receiving_Message_Will_Notify_Listener() {
             const string message = "/question What is 1+1?";
             const string output = "Thank you";
+            const string nullOutput = null;
+            const string exitCommand = "/exitQuestionBot";
 
             _testConsole.SetupSequence( x => x.ReadLine() )
                 .Returns( message )
                 .Returns( "/exitQuestionBot" );
 
             _testListener.Setup( x => x.ReceiveMessage( message ) ).Returns( output );
-            _testListener.Setup( x => x.ReceiveMessage( "/exitQuestionBot" ) ).Returns( output );
+            _testListener.Setup( x => x.ReceiveMessage( exitCommand ) ).Returns( nullOutput );
 
             _testEmitter = new MessageEmitter( _testConsole.Object );
             _testEmitter.Add( _testListener.Object );
             _testEmitter.Start();
 
             _testListener.Verify( x => x.ReceiveMessage( message ), Times.Exactly( 1 ) );
+            _testListener.Verify( x => x.ReceiveMessage( exitCommand ), Times.Exactly( 1 ) );
             _testConsole.Verify( x => x.WriteLine( output ), Times.Exactly( 1 ) );
         }
 
@@ -47,7 +50,6 @@ namespace QuestionBot.UnitTests.Model {
                 .Returns( "/exitQuestionBot" );
 
             _testListener.Setup( x => x.ReceiveMessage( message ) ).Returns( output );
-            _testListener.Setup( x => x.ReceiveMessage( "/exitQuestionBot" ) ).Returns( output );
 
             _testEmitter = new MessageEmitter( _testConsole.Object );
             _testEmitter.Start();
