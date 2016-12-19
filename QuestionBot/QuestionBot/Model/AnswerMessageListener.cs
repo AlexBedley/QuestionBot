dbb90b769b28e84f4639ed57e5c634a92b614b81
Steffanie Freeman <Steffanie.Freeman@D2L.com>
2016-12-19 10:49:57 -0500

    Begin work on AnswerMessageListener

---
+++ QuestionBot/QuestionBot/Model/AnswerMessageListener.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionBot.Model {
+    class AnswerMessageListener : IMessageListener {
+        private const string AnswerCommand = "/answer";
+        public const string ErrorMessage = "This answer or ID appears to be blank, please retry.";
+        private IStore _answerDataStore;
+
+
+        public AnswerMessageListener( IStore store ) {
+            _answerDataStore = store;
+        }
+
+        public string ReceiveMessage( string message ) {
+            string words = "a";
+            IRecord questionRecord;
+            int id;
+
+            if ( String.IsNullOrEmpty( message ) || !message.StartsWith( AnswerCommand ) ) {
+                return null;
+            }
+
+            string idWithAnswerText = message.Remove( 0, AnswerCommand.Length );
+            string[] splitIdWithAnswer = idWithAnswerText.Split( null );
+
+            if ( splitIdWithAnswer.Count() < 2 ) {
+                return ErrorMessage;
+            }
+
+            Int32.TryParse( splitIdWithAnswer[0], out id );
+            string answer = splitIdWithAnswer[1];
+
+            _answerDataStore.TryUpdateRecord( id, answer, out questionRecord );
+            outputMessage = "Question with ID " + id + " has been updated with your answer: " +
+                            answer;
+            return "";
+        }
+    }
+}
